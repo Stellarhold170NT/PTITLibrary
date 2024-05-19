958de8861d2e1930fde94f06d6fbf5e2a5d419d0
Nguyen Huy <99376592+Stellarhold170NT@users.noreply.github.com>
2024-05-19 22:06:21 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,115 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".introduce .intro-list");
+    const slideButtons = document.querySelectorAll(".introduce .slide-btn");
+    const scrollbarThumb = document.querySelector(
+        ".introduce .scrollbar-thumb"
+    );
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-btn" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            console.log(scrollAmount);
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    imageList.addEventListener("scroll", () => {
+        // updateScrollThumbPosition();
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition =
+            (scrollPosition / maxScrollLeft) *
+            (imageList.clientWidth - scrollbarThumb.offsetWidth);
+        console.log("${thumbPosition}px");
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    });
+};
+
+window.addEventListener("load", initSlider);
+
+var navbar = document.querySelector("nav");
+
+console.log(window.innerWidth + " " + window.innerHeight);
+
+// carousel
+
+window.addEventListener("resize", function () {
+    var carousel = document.querySelector(".introduce ul");
+    var width_carousel = carousel.clientWidth;
+    var height_carousel = width_carousel * 0.573921;
+    carousel.style.height = height_carousel + "px";
+});
+
+window.addEventListener("resize", function () {
+    var item = document.querySelector(".header .search");
+    console.log(item.clientWidth);
+});
+
+var listSubject = document.querySelectorAll("td");
+console.log("len" + listSubject.length);
+for (var i = 0; i < listSubject.length; i++) {
+    var tableData = listSubject[i];
+
+    var a = document.createElement("a");
+    var div = document.createElement("div");
+
+    a.href = "#!";
+    a.style.width = "100%";
+    a.style.height = "100%";
+    div.innerHTML = tableData.innerHTML;
+
+    tableData.innerHTML = "";
+    a.appendChild(div);
+    tableData.appendChild(a);
+
+    div.style.display = "flex";
+    div.style.flexDirection = "column";
+    div.style.alignItems = "center";
+    div.style.justifyContent = "center";
+    div.style.gap = "8px";
+
+    a.style.display = "flex";
+    a.style.alignItems = "center";
+    a.style.justifyContent = "center";
+}
+
+var viewDoc = document.querySelector(".view-doc");
+var viewDocLink = document.querySelector(".view-doc a");
+var aElements = document.querySelectorAll("td a");
+aElements.forEach((a) => {
+    a.addEventListener("click", function () {
+        viewDoc.setAttribute("class", "view-doc active");
+        viewDocLink.href = "#!";
+        var spanElement = a.querySelector("div > span");
+        var name = spanElement.innerHTML;
+        console.log("name = " + name);
+        $.ajax({
+            url: "data.json",
+            dataType: "json",
+            success: function (dataObject) {
+                dataObject.MonHocChung.forEach((monhoc) => {
+                    if (name === monhoc.name) {
+                        console.log(monhoc.link);
+                        viewDocLink.href = monhoc.link;
+                        viewDocLink.target = "_blank";
+                    }
+                });
+
+                dataObject.CongNgheThongTin.forEach((monhoc) => {
+                    if (name === monhoc.name) {
+                        console.log(monhoc.link);
+                        viewDocLink.href = monhoc.link;
+                        viewDocLink.target = "_blank";
+                    }
+                });
+            },
+        });
+    });
+});
+
+var viewCloseBtn = document.querySelector(".view-close-btn");
+
+viewCloseBtn.addEventListener("click", function () {
+    viewDoc.setAttribute("class", "view-doc");
+});
